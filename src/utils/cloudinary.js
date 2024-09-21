9a86fa483ace13e85737265bc28f4f8c16f00202
ahsanae2 <ahsanali.official085@gmail.com>
2024-09-21 13:24:34 +0500

    Added login route

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,11 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import dotenv from "dotenv";
 
-          
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnClouldinary = async (localFilePath) => {
