46cbf0089cae756506af625fb4c3710dfc1eb614
ahsanae2 <ahsanali.official085@gmail.com>
2024-10-14 18:43:53 +0500

    Error handling

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,21 +1,22 @@
-const asyncHandler = (requestHandler) => {
-  return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
-      next(error);
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+//       next(error);
+//     });
+//   };
+// };
+
+const asyncHandler = (fun) => async (req, res, next) => {
+  try {
+    await fun(req, res, next);
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      status: error.statusCode,
+      success: false,
+      message: error.message,
     });
-  };
+    console.log(error);
+  }
 };
 
 export default asyncHandler;
-
-// const asyncHandler = (fun) => async(req,res,next) =>{
-//     try{
-//         await fun(req,res,next)
-//     }catch(error){
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: error.message
-//         })
-//         console.log(error)
-//     }
-// }
