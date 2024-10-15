9f7b61e9734bd56b585f021e8e36f97cee6c3411
ahsanae2 <ahsanali.official085@gmail.com>
2024-10-15 23:59:40 +0500

    removed unused data

--- src/index.js
+++ src/index.js
@@ -41,25 +41,7 @@ const app = express();
 app.use(cors())
 const port = process.env.PORT || 3000;
 
-const github = {
-  id: 12,
-  title: "Brown Perfume",
-  description: "Royal_Mirage Sport Brown Perfu...",
-  price: 40,
-  discountPercentage: 15.66,
-  rating: 4,
-  stock: 52,
-  brand: "Royal_Mirage",
-  category: "fragrances",
-   thumbnail: "https://cdn.dummyjson.com/product-images/12/thumbnail.jpg",
-  images: [
-    "https://cdn.dummyjson.com/product-images/12/1.jpg",
-    "https://cdn.dummyjson.com/product-images/12/2.jpg",
-    "https://cdn.dummyjson.com/product-images/12/3.png",
-    "https://cdn.dummyjson.com/product-images/12/4.jpg",
-    "https://cdn.dummyjson.com/product-images/12/thumbnail.jpg",
-  ], 
-};
+
 
 const data = [
   {
